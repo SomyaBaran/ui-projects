7d91f502928a5f66532e833205215c9dc225d695
SomyaBaran <somyabaranwal6664@gmail.com>
2026-02-21 03:29:51 +0530

    init

--- stagehand/src/components/thirdPage.tsx
+++ stagehand/src/components/thirdPage.tsx
@@ -67,7 +67,7 @@ export default function ThirdPage() {
                 alignItems: "flex-end",
                 zIndex: 2,
             }}>
-            {/* // research tab */}
+                {/* // research tab */}
                 <div style={{
                     height: "8vh",
                     width: "19vw",
@@ -83,8 +83,53 @@ export default function ThirdPage() {
                 }}>
                     Research agent
                 </div>
-            </div>
 
+                {/* // pink triangle */}
+                <div style={{
+                    width: "64px",
+                    height: "64px",
+                    backgroundColor: "#f9a8c9",
+                    transform: "rotate(180deg)",
+                    clipPath: "polygon(0 0, 100% 0, 100% 100%)",
+                    // borderBottom: "2px solid black"
+                }}>
+                    <div style={{
+                        position: "absolute",
+                        inset: 0,
+                        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cline x1='0' y1='8' x2='8' y2='0' stroke='%23c8860a' stroke-width='0.8' stroke-opacity='0.35'/%3E%3Cline x1='-2' y1='2' x2='2' y2='-2' stroke='%23c8860a' stroke-width='0.8' stroke-opacity='0.35'/%3E%3Cline x1='6' y1='10' x2='10' y2='6' stroke='%23c8860a' stroke-width='0.8' stroke-opacity='0.35'/%3E%3C/svg%3E")`,
+                    }} />
+                </div>
+
+                {/* // Task agent  */}
+                <div style={{
+                    height: "8vh",
+                    width: "19vw",
+                    border: "2px dashed black",
+                    borderBottom: "2px solid black",
+                    marginTop: "16px",
+                    backgroundColor: "white",
+                    display: "flex",
+                    alignItems: "center",
+                    padding: "16px 36px",
+                    fontSize: "27px",
+                    fontWeight: "400",
+                }}>
+                </div>
+
+                <div style={{
+                    width: "64px",
+                    height: "64px",
+                    backgroundColor: "#eb734f",
+                    transform: "rotate(180deg)",
+                    clipPath: "polygon(0 0, 100% 0, 100% 100%)",
+                }}>
+                    <div style={{
+                        position: "absolute",
+                        inset: 0,
+                        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cline x1='0' y1='8' x2='8' y2='0' stroke='%23c8860a' stroke-width='0.8' stroke-opacity='0.35'/%3E%3Cline x1='-2' y1='2' x2='2' y2='-2' stroke='%23c8860a' stroke-width='0.8' stroke-opacity='0.35'/%3E%3Cline x1='6' y1='10' x2='10' y2='6' stroke='%23c8860a' stroke-width='0.8' stroke-opacity='0.35'/%3E%3C/svg%3E")`,
+                    }} />
+                </div>
+            </div>
 
             {/* // main white div  */}
             <div style={{
@@ -97,7 +142,8 @@ export default function ThirdPage() {
                 borderBottom: "none",
                 backgroundColor: "white",
             }}>
+
             </div>
-        </div>
+        </div >
     )
 }
