c5ae8f2e7740b916fb2b79b9784753470cc0d6e9
SomyaBaran <somyabaranwal6664@gmail.com>
2026-02-21 00:43:13 +0530

    init

--- stagehand/src/components/thirdPage.tsx
+++ stagehand/src/components/thirdPage.tsx
@@ -1,7 +1,7 @@
 export default function ThirdPage() {
     return (
         <div style={{
-            height: "90vh",
+            height: "100vh",
             width: "90vw",
             position: "relative",
             marginLeft: "5vw",
@@ -14,25 +14,29 @@ export default function ThirdPage() {
                 position: "absolute",
                 inset: 0,
                 backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cline x1='0' y1='8' x2='8' y2='0' stroke='%23c8860a' stroke-width='0.8' stroke-opacity='0.35'/%3E%3Cline x1='-2' y1='2' x2='2' y2='-2' stroke='%23c8860a' stroke-width='0.8' stroke-opacity='0.35'/%3E%3Cline x1='6' y1='10' x2='10' y2='6' stroke='%23c8860a' stroke-width='0.8' stroke-opacity='0.35'/%3E%3C/svg%3E")`,
-                zIndex: 0,
             }} />
 
             <div style={{
                 position: "absolute",
-                top: 0,
-                left: 0,
-                zIndex: 1,
                 border: "2px solid black",
                 height: "25vh",
                 width: "40vw",
                 backgroundColor: "white",
                 borderTop: "none",
                 borderLeft: "none",
-            }} />
+                fontSize: "50px",
+                fontWeight: "300",
+                paddingLeft: "85px",
+                paddingTop: "20px",
+                lineHeight: "1"
+            }}>
+                Automate anything <br />
+                in the browser, <br />
+                reliably
+            </div>
 
             <div style={{
                 position: "absolute",
-                top: 0,
                 left: "calc(40vw - 2px)",
                 zIndex: 1,
                 width: "64px",
@@ -44,8 +48,7 @@ export default function ThirdPage() {
 
             <div style={{
                 position: "absolute",
-                top: "calc(25vh - 2px)",  
-                left: 0,
+                top: "calc(25vh - 2px)",
                 zIndex: 1,
                 width: "78px",
                 height: "55px",
@@ -54,6 +57,47 @@ export default function ThirdPage() {
                 borderLeft: "none",
                 borderBottom: "2px solid black"
             }} />
+
+
+            <div style={{
+                position: "absolute",
+                top: "44.5vh",
+                left: "5vw",
+                display: "flex",
+                alignItems: "flex-end",
+                zIndex: 2,
+            }}>
+            {/* // research tab */}
+                <div style={{
+                    height: "8vh",
+                    width: "19vw",
+                    border: "2px solid black",
+                    borderBottom: "none",
+                    marginTop: "16px",
+                    backgroundColor: "white",
+                    display: "flex",
+                    alignItems: "center",
+                    padding: "16px 36px",
+                    fontSize: "27px",
+                    fontWeight: "400",
+                }}>
+                    Research agent
+                </div>
+            </div>
+
+
+            {/* // main white div  */}
+            <div style={{
+                height: "55vh",
+                width: "80vw",
+                border: "2px solid black",
+                position: "absolute",
+                top: "calc(44.5vh + 10vh - 2px)",
+                left: "5vw",
+                borderBottom: "none",
+                backgroundColor: "white",
+            }}>
+            </div>
         </div>
     )
 }
